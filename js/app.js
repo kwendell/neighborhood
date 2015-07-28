2e9e5b581204e143d8bf3f1e578f677718cb4acc
Kevin Duell <kdubzot9@gmail.com>
2015-07-27 20:02:08 -0700

    Populate model with different objects, keys.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var ViewModel = function() {
 
+
    var viewData = [
     {name:'Home'},
     {name:'History'},
@@ -12,10 +13,18 @@ var ViewModel = function() {
    var self = this;
    this.viewList = ko.observableArray([]);
 
-   viewData.forEach(function(viewItem) {
-
-      self.viewList.push(new View(viewItem));
-   });
+  // viewData.forEach(function() {
+       var homeView = new HomeView();
+       var view = new View();
+      // Push objects with keys onto the array.
+      /*
+      self.viewList[homeView.name()]=homeView;
+      self.viewList[view.name()]=view;
+      */
+      //var homeViewInstance = {homeView.name:homeView};
+      self.viewList.push({name:homeView.name,obj:homeView});
+      self.viewList.push({name:view.name,obj:view});
+  // });
 
    this.currentView = ko.observable(this.viewList()[0]);
 
@@ -30,12 +39,17 @@ var ViewModel = function() {
 };
 
 var View  = function(data) {
-   this.name = ko.observable(data.name);
-
-
+   this.name = ko.observable("View");
 
 };
 
+var HomeView = function(data) {
+   var api_key = "c76fa576c8399b0b88d14111dce6e3e8:18:72432843";
+  this.name=ko.observable("Home");
+  this.articlesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+"San Jose CA"+"&sort=newest&api-key="+api_key;
+  this.text = "Hiding in plain view of the Silicon Valley is Communications Hill.   Communications Hill is a San Jose neighborhood" +
+  " SouthEast of Willow Glen, North of Santa Teresa.";
 
+};
 
 ko.applyBindings(new ViewModel());
