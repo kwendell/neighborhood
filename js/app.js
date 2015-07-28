45bdd03c5a45545b97bb9ecb40545b6262350184
Kevin Duell <kdubzot9@gmail.com>
2015-07-28 00:20:50 -0700

    Got isHome(), isView() functions to work with bindings.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
+
+ var api_key = "c76fa576c8399b0b88d14111dce6e3e8:18:72432843";
 var ViewModel = function() {
 
 
-   var viewData = [
-    {name:'Home'},
-    {name:'History'},
-    {name:'Map/Places of Interest'},
-    {name:'other'},
 
-
-   ];
 
    var self = this;
    this.viewList = ko.observableArray([]);
@@ -25,14 +20,18 @@ var ViewModel = function() {
       self.viewList.push({name:homeView.name,obj:homeView});
       self.viewList.push({name:view.name,obj:view});
   // });
-
+   this.viewList()[0].isShowing=true;
    this.currentView = ko.observable(this.viewList()[0]);
 
+
+   this.isHome =  ko.computed(function() {var retval = "Home"==self.currentView().name();return retval;});
+   this.isView =  ko.computed(function() {var retval = "View"==self.currentView().name();return retval;});
 
 
    this.setView = function(clickedView) {
 
       self.currentView(clickedView);
+
 
    };
 
@@ -41,14 +40,18 @@ var ViewModel = function() {
 var View  = function(data) {
    this.name = ko.observable("View");
 
+
 };
 
 var HomeView = function(data) {
-   var api_key = "c76fa576c8399b0b88d14111dce6e3e8:18:72432843";
+
   this.name=ko.observable("Home");
   this.articlesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+"San Jose CA"+"&sort=newest&api-key="+api_key;
+
+
   this.text = "Hiding in plain view of the Silicon Valley is Communications Hill.   Communications Hill is a San Jose neighborhood" +
-  " SouthEast of Willow Glen, North of Santa Teresa.";
+  " between Willow Glen and Santa Teresa.  The area boasts a park, a walking trail, a vineyard and a popular " +
+  "public exercise area known as the Grand Staircase.  The neighborhood is steeped in a rich history of the valley's golden age of agriculture.";
 
 };
 
